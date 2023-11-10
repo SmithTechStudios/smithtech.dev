be7016b6d6224508f44a17f5d1624458cfb58e9c
Tom Smith <tom.smith@qunifi.com>
2023-11-10 16:30:55 +0000

    Remove stuff from website  until ready

--- app.vue
+++ app.vue
@@ -9,95 +9,12 @@
     </nav>
     <main class="">
       <div class="container mx-auto flex flex-col gap-4 items-center justify-center">
-        <h1 class="text-3xl">Creating Websites and Apps</h1>
-
 
         <img src="~/assets/logo.webp" class="h-56 rounded-3xl" alt="">
 
-
-        <img src="~/assets/undraw_progressive_app_m-9-ms.svg" class="h-48 mb-8" alt="">
+        <h1 class="text-5xl">Coming Soon....</h1>
       </div>
-      <div class="bg-[#283149] text-white flex flex-col items-center justify-center p-4">
-        <p class="text-2xl center mb-10">Hello we are SmithTech Studios</p>
 
-        <div class="flex gap-8">
-          <div>
-            <div
-              class="relative hover:scale-110 transition-all ease-in flex w-96 flex-col rounded-xl bg-[#DBEDF3] bg-clip-border text-gray-700 shadow-md">
-              <div
-                class="relative mx-4 mt-4 h-80 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700 shadow-lg">
-                <img src="https://api.dicebear.com/7.x/lorelei/svg?seed=TomASmith" alt="avatar" />
-              </div>
-              <div class="p-6 text-center">
-                <h4
-                  class="mb-2 block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-                  Tom Smith
-                </h4>
-                <p
-                  class="block bg-gradient-to-tr from-pink-600 to-pink-400 bg-clip-text font-sans text-base font-medium leading-relaxed text-transparent antialiased">
-                  CEO / Co-Founder
-                </p>
-                <p>
-                  Web Developer
-                </p>
-              </div>
-              <div class="flex justify-center gap-7 p-6 pt-2">
-                <a href="#facebook"
-                  class="block bg-gradient-to-tr from-blue-600 to-blue-400 bg-clip-text font-sans text-xl font-normal leading-relaxed text-transparent antialiased">
-                  <i class="fab fa-facebook" aria-hidden="true"></i>
-                </a>
-                <a href="#twitter"
-                  class="block bg-gradient-to-tr from-light-blue-600 to-light-blue-400 bg-clip-text font-sans text-xl font-normal leading-relaxed text-transparent antialiased">
-                  <i class="fab fa-twitter" aria-hidden="true"></i>
-                </a>
-                <a href="#instagram"
-                  class="block bg-gradient-to-tr from-purple-600 to-purple-400 bg-clip-text font-sans text-xl font-normal leading-relaxed text-transparent antialiased">
-                  <i class="fab fa-instagram" aria-hidden="true"></i>
-                </a>
-              </div>
-            </div>
-
-          </div>
-          <div>
-            <div
-              class="relative hover:scale-110 transition-all ease-in flex w-96 flex-col rounded-xl bg-[#DBEDF3] bg-clip-border text-gray-700 shadow-md">
-              <div
-                class="relative mx-4 mt-4 h-80 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700 shadow-lg">
-                <img src="https://api.dicebear.com/7.x/lorelei/svg?seed=PoppySmith" alt="avatar" />
-              </div>
-              <div class="p-6 text-center">
-                <h4
-                  class="mb-2 block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-                  Poppy Smith
-                </h4>
-                <p
-                  class="block bg-gradient-to-tr from-pink-600 to-pink-400 bg-clip-text font-sans text-base font-medium leading-relaxed text-transparent antialiased">
-                  CEO / Co-Founder
-                </p>
-                <p>
-                  Product Owner
-                </p>
-              </div>
-              <div class="flex justify-center gap-7 p-6 pt-2">
-                <a href="#facebook"
-                  class="block bg-gradient-to-tr from-blue-600 to-blue-400 bg-clip-text font-sans text-xl font-normal leading-relaxed text-transparent antialiased">
-                  <i class="fab fa-facebook" aria-hidden="true"></i>
-                </a>
-                <a href="#twitter"
-                  class="block bg-gradient-to-tr from-light-blue-600 to-light-blue-400 bg-clip-text font-sans text-xl font-normal leading-relaxed text-transparent antialiased">
-                  <i class="fab fa-twitter" aria-hidden="true"></i>
-                </a>
-                <a href="#instagram"
-                  class="block bg-gradient-to-tr from-purple-600 to-purple-400 bg-clip-text font-sans text-xl font-normal leading-relaxed text-transparent antialiased">
-                  <i class="fab fa-instagram" aria-hidden="true"></i>
-                </a>
-              </div>
-            </div>
-
-          </div>
-        </div>
-
-      </div>
     </main>
   </div>
   <NuxtPage />
